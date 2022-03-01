df098dc2fffa2f860b4e5d770063aa159b7d5a9b
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-01 14:52:57 +0200

    create a connection to mysql

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,27 @@ import cors from "cors";
 import { createConnection } from "typeorm";
 
 const main = async () => {
- const app = express()
- app.use(cors())
- app.use(express.json())
- 
+  await createConnection({
+    type: "mysql",
+    database: "GraphqlCRUD",
+    username: "root",
+    password: "963741",
+    logging: true,
+    synchronize: true,
+    entities: [],
+  });
+
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  //  app.use('/graphql', graphqlHTTP({
+  //    schema,
+  //    graphiql: true
+  //  }))
+
+  app.listen(3001, () => {
+    console.log("Server is up and running on port 3001");
+  });
 };
 
 main().catch((err) => {
