d75c66a39e8c9fe79d1f63778f656d3293f75ed0
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-04 12:09:06 +0200

    create CreateUser component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,20 @@
 import React, { useState } from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./App.css";
+import CreateUser from "./Components/CreateUser";
 
 function App() {
-  const [name, setName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-
   const client = new ApolloClient({
     uri: "http://localhost:5000/graphql",
     cache: new InMemoryCache(),
   });
 
   return (
-    <ApolloProvider client={client}>
-      <div className="createUser">
-        <input
-          type="text"
-          placeholder="name"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="username"
-          onChange={(event) => {
-            setUserName(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="password"
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-        <button>Create User</button>
-      </div>
-    </ApolloProvider>
+    <>
+      <ApolloProvider client={client}>
+        <CreateUser />
+      </ApolloProvider>
+    </>
   );
 }
 
--- client/src/Graphql/Mutation.ts
+++ client/src/Graphql/Mutation.ts
@@ -2,6 +2,10 @@ import { gql } from "@apollo/client";
 
 export const CREATE_USER = gql`
   mutation createUser($name: String!, $username: String!, $password: String!) {
-    createUser(name: $name, username: $username, password: $password)
+    createUser(name: $name, username: $username, password: $password) {
+      id
+      name
+      username
+    }
   }
 `;
